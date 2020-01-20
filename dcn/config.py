abba5aa600059c00ee719fa9321776f6c7e83c0b
Akash SM <akashsm@ce.iitr.ac.in>
2020-01-21 02:49:04 +0530

    updated

--- dcn/config.py
+++ dcn/config.py
@@ -12,7 +12,7 @@ class GeneralConfig:
 
         self.context_len = 600
         self.question_len = 30
-        self.word_emb_path = os.path.join(
+        self.word_embedding_path = os.path.join(
             ROOT, "dcn", "glove", "word_embeddings.npy")
         self.word2id_path = os.path.join(ROOT, "dcn", "glove", "word2id.pkl")
         self.id2word_path = os.path.join(ROOT, "dcn", "glove", "id2word.pkl")
@@ -37,20 +37,12 @@ class ModelConfig:
 
         self.hidden_dim = 200
         self.embedding_dim = 300
-
         # vector with zeros for unknown words
         self.decoding_steps = 4
         self.maxout_pool_size = 16
-
         self.lr = 0.001
         self.dropout_ratio = 0.15
-
-        self.max_grad_norm = 5.0
         self.batch_size = 32
         self.num_epochs = 50
-
-        self.print_every = 100
-        self.save_every = 50000000
-        self.eval_every = 1000
+        self.print_frequency = 100
         # regularization constant
-        self.reg_lambda = 0.00007
